e35c34706feea4b95a66884e5935dcd5ba4d50ab
sakitibi <sakitibi10nin@icloud.com>
2025-12-13 15:43:14 +0900

    Merge remote changes

--- bin/main/com/example/seerplugins/SeerPlugins.kt
+++
@@ -1,9 +0,0 @@
-package com.example.seerplugins
-
-import net.fabricmc.api.ModInitializer
-
-object SeerPlugins : ModInitializer {
-    override fun onInitialize() {
-        TickHandler.register()
-    }
-}
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "seer.plugins"
-version = "3.1.0.0"
+version = "3.2.0.0"
 
 repositories {
     maven {
